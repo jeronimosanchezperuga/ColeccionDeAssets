587488ea7a5416beb7ed07a9061faa9bbe10122f
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2023-02-06 15:45:15 -0300

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController2D characterController;
+    [SerializeField]
+    Rigidbody2D rb;
+    public float runSpeed = 40f;
+    float horizontalMove;
+    bool jump = false;
+    bool crouch = false;
+    public bool onStair = false;
+    public float stairX;
+    [SerializeField]
+    private float stairSpeed;
+    public float minY;
+    public float maxY;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        float verticalMove = Input.GetAxis("Vertical") * stairSpeed;
+
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+
+        
+    } 
+
+    void FixedUpdate()
+    {
+        characterController.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Coleccion-de-Assets
+ 
