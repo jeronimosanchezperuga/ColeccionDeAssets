09278a5ca5a4ef1675ab6060c67e4c3b036981ee
jsanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2023-02-07 12:47:47 -0300

    .

---
+++ Assets/PickUpItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    [SerializeField] int prizeAmount;
+    [SerializeField] GameObject fxFeedback;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PickedUpBehavior();
+        }
+    }
+
+    void PickedUpBehavior()
+    {
+        Destroy(gameObject);
+        GameObject fxGO = Instantiate(fxFeedback,gameObject.transform.position,Quaternion.identity);
+        Destroy(fxGO, 1);
+    }
+
+    
+}
